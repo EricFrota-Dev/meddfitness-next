3a3c36d67058f04fc450116f92aa77b6ccd37f34
Eric Alves Frota <ericeaf@hotmail.com>
2025-08-18 22:06:09 -0300

    fix route error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   output: "export",
   images: { unoptimized: true },
+  basePath: isProd ? "/meddfitness-next" : "",
+  assetPrefix: isProd ? "/meddfitness-next" : "",
+  trailingSlash: true, // importante para exportação estática
 };
 
 module.exports = nextConfig;
